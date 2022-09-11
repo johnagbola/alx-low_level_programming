b9817f7506cc8d1e6df8d3c23fdf56f019492ce3
YJScript <oluwatobiloba.agbola@gmail.com>
2022-09-11 10:17:47 -0700

    2. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+	char m;
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
