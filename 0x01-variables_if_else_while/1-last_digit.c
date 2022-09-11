77707e1ab04e81421fa82eaf9571fc064207b960
YJScript <oluwatobiloba.agbola@gmail.com>
2022-09-11 09:45:38 -0700

    1. The last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assign a random number to variable n, when executed.
+ * function prints if number is greater than 5, equals to 0
+ * also func prints less than 6 and not equal to 0.
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+
+        printf("Last digit of %d is %d ", n, n % 10);
+        if (n % 10 > 5)
+        {
+                printf("and is greater than 5\n" );
+        }
+        else if (n % 10 == 0)
+        {
+                printf("and is 0\n" );
+        }
+        else if (n % 10 < 6 && n % 10 != 0)
+        {
+                printf("and is less than 6 and not 0\n" );
+        }
+        return (0);
+}
