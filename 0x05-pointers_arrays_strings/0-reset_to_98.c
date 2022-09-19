246d77bb85d26c54973c441ee361e9954f2917bd
YJScript <oluwatobiloba.agbola@gmail.com>
2022-09-19 16:30:46 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Resets the value for a variable to 98
+ * @n: a pointer dereference for changing the value of the variable
+ * 
+ * Return: 0 always success.
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+0x05. C - Pointers, arrays and strings
+This folder introduce pointers and arrays, Data structure.
+0-reset_to_98.c: A function that takes a pointer to an int as parameter and updates the value it points to to 98.
+
