8e480e22243d0a79e5a41d32bfe41f9765ed97d0
YJScript <oluwatobiloba.agbola@gmail.com>
2022-09-22 10:31:50 -0700

    12. Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,31 +1,26 @@
-#include "main.h"
-#include <stdio.h>
 /**
- * print_line - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
  *
  * Return: void
  */
-
-void print_line(char *c, int s, int l)
+void print_buffer(char *b, int size)
 {
-int j, k;
-for (j = 0; j <= 9; j++)
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
 {
-if (j <= s)
-printf("%02x", c[l * 10 + j]);
-else
-printf("  ");
-if (j % 2)
-putchar(' ');
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
 }
-for (k = 0; k <= s; k++)
-{
-if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-putchar(c[l * 10 + k]);
 else
-putchar('.');
+{
+print_line(b, size % 10 - 1, i);
 }
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
