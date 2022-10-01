c795afac951e2eef7957f139bb50d1350b94cdb5
YJScript <oluwatobiloba.agbola@gmail.com>
2022-09-30 17:38:52 -0700

    1. Silence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - print the name of the program
+ * @argc: Argument count
+ * @argv: Arguments Value
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv; /*Ignore argv*/
+	printf("%i\n", argc - 1);
+
+	return (0);
+}
