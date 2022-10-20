05698df0022f35bc7c90a56cffe79890ffe80a49
YJScript <oluwatobiloba.agbola@gmail.com>
2022-10-20 02:32:29 -0700

    A function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,14 +1,16 @@
 #include "lists.h"
+
 /**
- * print_list - This func prints list.
+ * print_list - prints list.
  * @h: param list
  * Return: struct
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t nodelen;
+	size_t l_node;
 
-	nodelen = 0;
+	l_node = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -16,7 +18,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nodelen++;
+		l_node++;
 	}
-	return (nodelen);
+	return (l_node);
 }
